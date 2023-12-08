067c1b88e21c4d008d40bc74f65d72785ac409aa
Zenith <draqonfyre@proton.me>
2023-12-08 14:24:40 -0600

    Added getters for Objects

--- src/game/object.rs
+++ src/game/object.rs
@@ -17,3 +17,37 @@ pub enum Object {
         label: Option<u8>,
     },
 }
+
+impl Object {
+    pub fn pos(&self) -> &Point {
+        match self {
+            Object::Airport { pos, .. } => pos,
+            Object::Beacon { pos, .. } => pos,
+            Object::Exit { pos, .. } => pos,
+        }
+    }
+
+    pub fn direction(&self) -> Option<&Direction> {
+        match self {
+            Object::Airport { direction, .. } => Some(direction),
+            Object::Beacon { .. } => None,
+            Object::Exit { direction, .. } => Some(direction),
+        }
+    }
+
+    pub fn label(&self) -> Option<&u8> {
+        match self {
+            Object::Airport { label, .. } => label.as_ref(),
+            Object::Beacon { label, .. } => label.as_ref(),
+            Object::Exit { label, .. } => label.as_ref(),
+        }
+    }
+
+    pub fn type_str(&self) -> String {
+        match self {
+            Object::Airport { .. } => "airport".to_owned(),
+            Object::Beacon { .. } => "beacon".to_owned(),
+            Object::Exit { .. } => "exit".to_owned(),
+        }
+    }
+}
