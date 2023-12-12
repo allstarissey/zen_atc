adee59a6a223a4557a3fb702c6c58f4083697369
Zenith <draqonfyre@proton.me>
2023-12-12 16:51:40 -0600

    Removed second impl block for App & derived Debug

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -8,6 +8,7 @@ use std::{fs, path::PathBuf};
 
 use self::{command::Command, map::Map, plane::Plane};
 
+#[derive(Debug)]
 pub struct App<'a> {
     map: Map,
     planes: Vec<Plane>,
@@ -29,19 +30,5 @@ impl App<'_> {
 
     pub fn dimensions(&self) -> (&u16, &u16) {
         (self.map.width(), self.map.height())
-    }
-}
-
-impl App<'_> {
-    pub fn new(path: PathBuf) -> Result<Self, Box<dyn std::error::Error>> {
-        let map: Map = serde_json::from_str(
-            fs::read_to_string(path)?
-            .as_str())?;
-
-        Ok(Self {
-            map,
-            planes: Vec::new(),
-            commands: Vec::new(),
-        })
     }
 }
