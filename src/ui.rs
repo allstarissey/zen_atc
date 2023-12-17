02dec6affe1b43bf758394a26181c858da560974
Zenith <draqonfyre@proton.me>
2023-12-17 15:31:56 -0700

    UI now includes CommandWriter

--- src/ui.rs
+++ src/ui.rs
@@ -1,6 +1,6 @@
 use ratatui::{
     layout::{Alignment, Constraint, Direction as LDirection, Layout},
-    widgets::{Block, Borders},
+    widgets::{Block, Borders, Paragraph},
     Frame,
 };
 
@@ -12,6 +12,7 @@ pub fn ui(frame: &mut Frame, app: &App) {
 
     let pad_width = (frame_width - map_width) / 2;
 
+    // Chunks
     let chunks = Layout::default()
         .direction(LDirection::Vertical)
         .constraints([
@@ -35,15 +36,18 @@ pub fn ui(frame: &mut Frame, app: &App) {
         .constraints([Constraint::Percentage(75), Constraint::Percentage(25)])
         .split(chunks[3]);
 
+    // Blocks
     let radar_block = make_block("Radar");
     let command_list_block = make_block("Commands");
     let plane_list_block = make_block("Planes");
     let command_block = make_block("Command");
 
+    let command_writer = Paragraph::new(app.cur_command()).block(command_block);
+
     frame.render_widget(radar_block, upper_chunks[1]);
     frame.render_widget(command_list_block, lower_chunks[0]);
     frame.render_widget(plane_list_block, lower_chunks[1]);
-    frame.render_widget(command_block, chunks[4]);
+    frame.render_widget(command_writer, chunks[4]);
 }
 
 fn make_block(title: &str) -> Block {
